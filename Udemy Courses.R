d7cd79816275ad96d8fdf58d25680e4337ed9a5d
lancer79 <lancerbrown79@gmail.com>
2020-03-18 11:08:22 -0700

    Vectorized operations

--- Udemy Courses.R
+++ Udemy Courses.R
@@ -193,6 +193,37 @@ w[-9]
 w[-4]
 w[-6]
 
+x <- rnorm(5)
+x
+#R specific programming loop
+for(i in x) {
+        print(i)
+}
+
+print(x[1])
+print(x[2])
+print(x[3])
+
+#conventional programming loop
+for(j in 1:5){
+        print(x[j])
+}
+
+#--------2nd part
+
+N <- 100
+a <- rnorm(N)
+b <- rnorm(N)
+
+#Vectorized approach (Faster than de-vectorized)
+c <- a * b
+
+#De-vectorized approach
+d <- rep(NA, N)
+for(i in 1:N){
+        d[i] <- a[i] * b[i]
+}
+
 
 
 
